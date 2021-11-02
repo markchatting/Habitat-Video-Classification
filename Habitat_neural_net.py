078fb5a60f113dfb73b5c640b13f070ff2f0487b
markchatting <57613411+markchatting@users.noreply.github.com>
2021-11-02 14:33:52 +0300

    Update Habitat_neural_net.py

--- Habitat_neural_net.py
+++ Habitat_neural_net.py
@@ -33,20 +33,6 @@ from os import path
 
 ########################################################################################################
 
-dir_dir = '/Users/mark/QU/People_stuff/Josh/RLC_videos_Sep2020/Videos'
-dir_dir2 = '/Users/mark/QU/People_stuff/Josh/RLC_videos_Sep2020/Vid_Images_test'
-for filename in os.listdir(dir_dir):
-    vidcap = cv2.VideoCapture(os.path.join(dir_dir, filename))
-    success, image = vidcap.read()
-    count = 0
-    while success:
-        cv2.imwrite(os.path.join(dir_dir2, filename[0:-5], filename[0:-5] + "frame%d.jpg") % count, image)
-        success, image = vidcap.read()
-        print('Read a new frame: ', success)
-        count += 1
-
-########################################################################################################
-
 
 #  GENERATE MORE IMAGES FROM PHOTOS TAKEN BY ROTATING, ZOOMING ETC.
 
