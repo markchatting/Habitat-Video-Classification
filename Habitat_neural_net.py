b1ee48e6fdcd9c21102deceef4fb08073bc2cad9
markchatting <57613411+markchatting@users.noreply.github.com>
2021-11-18 08:57:40 +0300

    Update Habitat_neural_net.py

--- Habitat_neural_net.py
+++ Habitat_neural_net.py
@@ -66,20 +66,6 @@ for folder_name in os.listdir(dir_dir):
 
 ########################################################################################################
 
-#test_dir = '/Users/mark/QU/People_stuff/Josh/RLC_videos_Sep2020/labelled_copy/train'
-#train_dir = '/Users/mark/QU/People_stuff/Josh/RLC_videos_Sep2020/labelled_copy/test'
-
-
-train_dir = '/Users/mark/QU/People_stuff/Josh/RLC_videos_Sep2020/labelled_copy/train'
-test_dir = '/Users/mark/QU/People_stuff/Josh/RLC_videos_Sep2020/labelled_copy/test'
-for dirname in os.listdir(train_dir):
-    from_dir = os.path.join(train_dir + '/' + dirname + '/')
-    dest_dir = os.path.join(test_dir + '/' + dirname + '/')
-    for filename in os.listdir(from_dir)[::5]:
-        shutil.move(from_dir + filename, dest_dir)
-
-########################################################################################################
-
 TRAINING_DIR = "/Users/mark/QU/People_stuff/Josh/RLC_videos_Sep2020/labelled_copy/train"
 training_datagen = ImageDataGenerator(
     rescale=1. / 255,
